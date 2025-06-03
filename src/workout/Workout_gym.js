a4e0ba52a38581e719bbf8d80e72a34715b4a8bf
dongukkk <kdu0816@gmail.com>
2025-06-03 13:11:26 +0900

    로직 수정 및 태그 디자인 변경

--- src/workout/Workout_gym.js
+++ src/workout/Workout_gym.js
@@ -1,5 +1,5 @@
 import './Workout_gym.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WorkoutModal from './components/WorkoutModal';
 import workoutData from './data/workoutData';
 
@@ -9,15 +9,23 @@ function Workout_gym() {
 
     const [showModal, setShowModal] = useState(false);
 
-    let [workoutDatas,setWorkoutDatas] = useState(workoutData);
+    let [workoutDatas, setWorkoutDatas] = useState(workoutData);
 
-    let [dataIndex,setDataIndex] = useState(0);
+    let [dataIndex, setDataIndex] = useState(0);
 
-    let workout_tags = ['모든 운동', '덤벨', '바벨', '스미스머신','케이블','맨몸'];
+    let workout_tags = ['모든 운동', '덤벨', '바벨', '스미스머신', '케이블', '맨몸'];
 
     let [selectedTag, setSelectedTag] = useState(workout_tags[0]);
 
-    let imageUrl = ['/images/gym_main.jpg','/images/dumbbell_main.png','/images/barbell_main.jpg','/images/smithMachine_main.jpg','/images/cable_main.jpg','/images/freeweight_main.jpg'];
+    let imageUrl = ['/images/gym_main.jpg', '/images/dumbbell_main.png', '/images/barbell_main.jpg', '/images/smithMachine_main.jpg', '/images/cable_main.jpg', '/images/freeweight_main.jpg'];
+
+    const [backgroundImage, setBackgroundImage] = useState(imageUrl[0]);
+
+    useEffect(() => {
+        const index = workout_tags.indexOf(selectedTag);
+        setBackgroundImage(imageUrl[index]);
+    }, [selectedTag]);
+
 
     return (
         <>
@@ -28,45 +36,45 @@ function Workout_gym() {
             <div className='workout-container'>
                 <div className='workout-tag-buttons'>
                     {
-                        workout_tags.map((name, index)=>{
-                            
-                            return <div className={`workout-tag ${selectedTag === name ? 'workout-tag-selected' : ''}`} onClick={()=>{
+                        workout_tags.map((name, index) => {
+
+                            return <div key={index} className={`workout-tag ${selectedTag === name ? 'workout-tag-selected' : ''}`} onClick={() => {
                                 setSelectedTag(workout_tags[index]);
-                                let workoutGrid = document.getElementsByClassName('workout-grid')[0];
-                                workoutGrid.style.backgroundImage = "url('" + imageUrl[index] + "')";
+                                // let workoutGrid = document.getElementsByClassName('workout-grid')[0];
+                                // workoutGrid.style.backgroundImage = "url('" + imageUrl[index] + "')";
                             }}>
                                 {name}
                             </div>
-                    })
-                }
+                        })
+                    }
                 </div>
 
-                <div className='workout-grid'>
-                    {workoutDatas.map((name, index)=>{
-                        if (selectedTag === "모든 운동"){
-                            return <div className='workout-card' onClick={()=>{
-                            setDataIndex(index);
-                            setShowModal(true);
+                <div className='workout-grid' style={{ backgroundImage: `url(${backgroundImage})` }}>
+                    {workoutDatas.map((name, index) => {
+                        if (selectedTag === "모든 운동") {
+                            return <div key={index} className='workout-card' onClick={() => {
+                                setDataIndex(index);
+                                setShowModal(true);
                             }}>{name.title}</div>
                         }
 
-                        if (workoutDatas[index].tool===selectedTag){
-                            return <div className='workout-card' onClick={()=>{
-                            setDataIndex(index);
-                            setShowModal(true);
+                        if (workoutDatas[index].tool === selectedTag) {
+                            return <div key={index} className='workout-card' onClick={() => {
+                                setDataIndex(index);
+                                setShowModal(true);
                             }}>{name.title}</div>
                         }
-                        
+
                     })
-                        
+
                     }
-                    
-                    
+
+
                 </div>
             </div>
 
             {showModal && (
-                <WorkoutModal closeModal={() => setShowModal(false)} workoutData = {workoutDatas[dataIndex]} />
+                <WorkoutModal closeModal={() => setShowModal(false)} workoutData={workoutDatas[dataIndex]} />
             )}
         </>
     );
