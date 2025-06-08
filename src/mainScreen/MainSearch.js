5e0907b268f3db15a58698153e99a3ac439cc3f8
Dongukkk <kdu0816@gmail.com>
2025-06-08 15:53:43 +0900

    통합 검색 결과 클릭시 모달창 출력력

--- src/mainScreen/MainSearch.js
+++ src/mainScreen/MainSearch.js
@@ -6,6 +6,7 @@ import aeroData from "../workout/data/aeroData";
 import stretchData from "../workout/data/stretchData";
 import allPosts from "../board/Posts";
 import { useNavigate, useSearchParams } from "react-router";
+import WorkoutModal from '../workout/components/WorkoutModal';
 
 
 function MainSearch() {
@@ -13,53 +14,62 @@ function MainSearch() {
   const navigate = useNavigate();
 
   const [query, setQuery] = useState(searchParams.get("q") || "");
-  const [results1, setResults1] = useState([]);
-  const [results2, setResults2] = useState([]);
-  const [results3, setResults3] = useState([]);
-  const [results4, setResults4] = useState([]);
+  const [resultsGym, setResultsGym] = useState([]);
+  const [resultsAero, setResultsAero] = useState([]);
+  const [resultsStretch, setResultsStretch] = useState([]);
+  const [resultsBoard, setResultsBoard] = useState([]);
   const [isSearched, setIsSearched] = useState(false);
+
+  const [selectedWorkout, setSelectedWorkout] = useState(null); // 선택된 운동
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열림 여부
+
+
+  const handleWorkoutClick = (item) => {
+    setSelectedWorkout(item);
+    setIsModalOpen(true);
+  };
 
   useEffect(() => {
     const currentQuery = searchParams.get("q")?.trim().toLowerCase() || "";
 
     if (!currentQuery) {
-      setResults1([]);
-      setResults2([]);
-      setResults3([]);
-      setResults4([]);
+      setResultsGym([]);
+      setResultsAero([]);
+      setResultsStretch([]);
+      setResultsBoard([]);
       setIsSearched(false);
       return;
     }
 
-    const filtered1 = workoutData.filter((item) =>
+    const filteredGym = workoutData.filter((item) =>
       (item.title + item.content + item.tool + item.area)
         .toLowerCase()
         .includes(currentQuery)
     );
 
-    const filtered2 = aeroData.filter((item) =>
+    const filteredAero = aeroData.filter((item) =>
       (item.title + item.content + item.tool + item.area)
         .toLowerCase()
         .includes(currentQuery)
     );
 
-    const filtered3 = stretchData.filter((item) =>
+    const filteredStretch = stretchData.filter((item) =>
       (item.title + item.content + item.tool + item.area)
         .toLowerCase()
         .includes(currentQuery)
     );
 
-    const filtered4 = allPosts.all.filter((item) =>
+    const filteredBoard = allPosts.all.filter((item) =>
       (item.title + item.writer + item.type)
         .toLowerCase()
         .includes(currentQuery)
     );
 
     // 항상 새로 덮어쓰기
-    setResults1(filtered1);
-    setResults2(filtered2);
-    setResults3(filtered3);
-    setResults4(filtered4);
+    setResultsGym(filteredGym);
+    setResultsAero(filteredAero);
+    setResultsStretch(filteredStretch);
+    setResultsBoard(filteredBoard);
     setIsSearched(true);
   }, [searchParams]); // URL 변경 감지
 
@@ -74,86 +84,101 @@ function MainSearch() {
   };
 
   return (
-  <div className="mainSearch-wrapper">
-    <h1 className="mainSearch-title">운동 검색</h1>
+    <div className="mainSearch-wrapper">
+      <h1 className="mainSearch-title">운동 검색</h1>
 
-    <form onSubmit={handleSearchSubmit} className="mainSearch-form">
-      <input
-        type="text"
-        placeholder="운동명, 도구, 부위 등 입력"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        className="mainSearch-input"
-      />
-      <button type="submit" className="mainSearch-button">
-        검색
-      </button>
-    </form>
+      <form onSubmit={handleSearchSubmit} className="mainSearch-form">
+        <input
+          type="text"
+          placeholder="운동명, 도구, 부위 등 입력"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="mainSearch-input"
+        />
+        <button type="submit" className="mainSearch-button">
+          검색
+        </button>
+      </form>
 
-    <hr className="mainSearch-divider" />
+      <hr className="mainSearch-divider" />
 
-    <div className="mainSearch-results">
-      {isSearched && <h1 className="mainSearch-section-title">운동 검색 결과</h1>}
+      <div className="mainSearch-results">
+        {isSearched && <h1 className="mainSearch-section-title">운동 검색 결과</h1>}
 
-      {/* 근력 운동 */}
-      {isSearched && <h2 className="mainSearch-subtitle">근력 운동</h2>}
-      {results1.length > 0 ? (
-        <ul className="mainSearch-list">
-          {results1.map((item) => (
-            <li key={`${item.id}-${item.title}`} className="mainSearch-item">
-              <strong>{item.title}</strong> ({item.tool} / {item.area})
-            </li>
-          ))}
-        </ul>
-      ) : (
-        isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
-      )}
+        {/* 근력 운동 */}
+        {isSearched && <h2 className="mainSearch-subtitle">근력 운동</h2>}
+        {resultsGym.length > 0 ? (
+          <ul className="mainSearch-list">
+            {resultsGym.map((item) => (
+              <li key={`${item.id}-${item.title}`} className="mainSearch-item" onClick={() => {
+                handleWorkoutClick(item)
+              }}>
+                <strong>{item.title}</strong> ({item.tool} / {item.area})
+              </li>
+            ))}
+          </ul>
+        ) : (
+          isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
+        )}
 
-      {/* 유산소 운동 */}
-      {isSearched && <h2 className="mainSearch-subtitle">유산소 운동</h2>}
-      {results2.length > 0 ? (
-        <ul className="mainSearch-list">
-          {results2.map((item) => (
-            <li key={`${item.id}-${item.title}`} className="mainSearch-item">
-              <strong>{item.title}</strong> ({item.tool} / {item.area})
-            </li>
-          ))}
-        </ul>
-      ) : (
-        isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
-      )}
+        {/* 유산소 운동 */}
+        {isSearched && <h2 className="mainSearch-subtitle">유산소 운동</h2>}
+        {resultsAero.length > 0 ? (
+          <ul className="mainSearch-list">
+            {resultsAero.map((item) => (
+              <li key={`${item.id}-${item.title}`} className="mainSearch-item" onClick={() => {
+                handleWorkoutClick(item)
+              }}>
+                <strong>{item.title}</strong> ({item.tool} / {item.area})
+              </li>
+            ))}
+          </ul>
+        ) : (
+          isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
+        )}
 
-      {/* 스트레칭 */}
-      {isSearched && <h2 className="mainSearch-subtitle">스트레칭</h2>}
-      {results3.length > 0 ? (
-        <ul className="mainSearch-list">
-          {results3.map((item) => (
-            <li key={`${item.id}-${item.title}`} className="mainSearch-item">
-              <strong>{item.title}</strong> ({item.tool} / {item.area.join(', ')})
-            </li>
-          ))}
-        </ul>
-      ) : (
-        isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
-      )}
+        {/* 스트레칭 */}
+        {isSearched && <h2 className="mainSearch-subtitle">스트레칭</h2>}
+        {resultsStretch.length > 0 ? (
+          <ul className="mainSearch-list">
+            {resultsStretch.map((item) => (
+              <li key={`${item.id}-${item.title}`} className="mainSearch-item" onClick={() => {
+                handleWorkoutClick(item)
+              }}>
+                <strong>{item.title}</strong> ({item.tool} / {item.area.join(', ')})
+              </li>
+            ))}
+          </ul>
+        ) : (
+          isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
+        )}
 
-      {/* 게시판 */}
-      {isSearched && <h1 className="mainSearch-section-title">게시판 검색 결과</h1>}
-      {isSearched && <h2 className="mainSearch-subtitle">모든 게시판</h2>}
-      {results4.length > 0 ? (
-        <ul className="mainSearch-list">
-          {results4.map((item) => (
-            <li key={`${item.id}-${item.title}`} className="mainSearch-item">
-              <strong>{item.title}</strong> ({item.writer} / {item.date})
-            </li>
-          ))}
-        </ul>
-      ) : (
-        isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
+        {/* 게시판 */}
+        {isSearched && <h1 className="mainSearch-section-title">게시판 검색 결과</h1>}
+        {isSearched && <h2 className="mainSearch-subtitle">모든 게시판</h2>}
+        {resultsBoard.length > 0 ? (
+          <ul className="mainSearch-list">
+            {resultsBoard.map((item) => (
+              <li key={`${item.id}-${item.title}`} className="mainSearch-item">
+                <strong>{item.title}</strong> ({item.writer} / {item.date})
+              </li>
+            ))}
+          </ul>
+        ) : (
+          isSearched && <p className="mainSearch-empty">검색 결과가 없습니다.</p>
+        )}
+      </div>
+      {isModalOpen && selectedWorkout && (
+        <WorkoutModal
+          closeModal={() => {
+            setIsModalOpen(false);
+            setSelectedWorkout(null);
+          }}
+          workoutData={selectedWorkout}
+        />
       )}
     </div>
-  </div>
-);
+  );
 
 }
 
